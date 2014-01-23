1fe308c5923f0aeb5f68fc1eae4e8d9c29a4fcd4
vagrant <vagrant@precise32.(none)>
2014-01-23 18:43:18 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -13,6 +13,50 @@ function initializePage() {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
 
+	$("a.thumbnail").click(projectClick);
+	$("#testjs").click(buttonClick);
+	$("#submitBtn").click(submitClick);
+
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+function submitClick(e) {
+	console.log($(".form-control#project").val());
+	var selectedproject = $(".form-control#project").val();
+	$(selectedproject).animate({
+		width: $(".form-control#width").val()
+	});
+
+	var newDescription = $(".form-control#description").val();
+	$(selectedproject + " .project-description").text(newDescription);
+}
+
+function buttonClick(e) {
+	$(this).text("Pressed");
+	$(".jumbotron p").toggleClass("active");
+}
+
+function projectClick(e) {
+	e.preventDefault();
+
+	$(this).css("background-color", "#7fff00");
+
+	var projectTitle = $(this).find("p").text();
+	//console.log(projectTitle);
+
+	var jumbotronHeader = $(".jumbotron h1");
+	//console.log(jumbotronHeader);
+	jumbotronHeader.text(projectTitle);
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	}
+	else {
+		$(".project-description").fadeToggle();
+	}
+
+
 }
